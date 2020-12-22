48dbb97e4889052a97e76e9a45f3214d3d1272aa
pghie87 <76475961+pghie87@users.noreply.github.com>
2020-12-22 21:27:04 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Emir Salma Prayogi
+          Emir Salma Prayogi Masya Allah
         </a>
       </header>
     </div>
